65b477aa31e7edaa71fcb8aae02b57a2d59f03d5
tiendo.vn <tiendo.vn@b1b9296c-9d0c-081c-44bb-1419d4cd2576>
2012-08-14 04:21:54 +0000

    import info paper to caculat

--- AND/src/uit/tkorg/and/utils/ReadXML.java
+++ AND/src/uit/tkorg/and/utils/ReadXML.java
@@ -12,6 +12,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import java.io.File;
 import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.List;
 import uit.tkorg.and.models.Author;
 import uit.tkorg.and.models.Publication;
@@ -22,9 +23,10 @@ import uit.tkorg.and.models.Publication;
  */
 public class ReadXML {
     
-    public Publication importPubFromXML (String fileName)
+    public static Publication importPubFromXML (String fileName)
     {
-        Publication result = null;
+        Publication result=null;
+        List<Author> coauthor=null;
         Author mainAuthor = null;
        
          try {
@@ -34,23 +36,47 @@ public class ReadXML {
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		Document doc = dBuilder.parse(fXmlFile);
 		doc.getDocumentElement().normalize();
-                
-                NodeList authorList = doc.getElementsByTagName("author");
+                result = new Publication();
+                //Read Paper info
+                Element rootElement = doc.getDocumentElement();
+                result.setDigitalName(getTagValue("digital", rootElement));
+                result.setNameSeach(getTagValue("nameseach", rootElement));
+                result.setJournal(getTagValue("journal", rootElement));
+                result.setPubAbstract(getTagValue("abstract", rootElement));
+                result.setPubKeywords(getTagValue("keywords", rootElement));
+                result.setPubTitle(getTagValue("title", rootElement));
+                result.setPubYearPublish(getTagValue("year", rootElement));
+                result.setPublisher(getTagValue("publisher", rootElement));
+                result.setConference(getTagValue("conference", rootElement));
+                // Read author info
+             
+                coauthor = new ArrayList<Author>();
+               NodeList authorList = doc.getElementsByTagName("author");
                for (int temp = 0; temp < authorList.getLength(); temp++) {
-                   
-                   Node nNode = authorList.item(temp);
-   
-		   if (nNode.getParentNode().getNodeName().equals("main")){
-		      Element eElement = (Element) nNode;
-		      mainAuthor.setAuthorName(getTagValue("name", eElement));
-		      mainAuthor.setAuthorAfflication(getTagValue("afflication", eElement));
+                  Node nNode = authorList.item(temp);
+                  Element eElement = (Element) nNode;
+                  if(!getTagValue("name", eElement).equals("")){
+                      mainAuthor = new Author();
+                      mainAuthor.setAuthorName(getTagValue("name", eElement));
+                      mainAuthor.setAuthorAfflication(getTagValue("afflication", eElement));
+                      if(eElement.hasAttribute("email"))
                       mainAuthor.setAuthorEmail(getTagValue("email", eElement));
-		      mainAuthor.setAuthorOrder(Integer.parseInt(getTagValue("order", eElement)));
+                      if(eElement.hasAttribute("order")&&getTagValue("order", eElement)!=null)
+                      mainAuthor.setAuthorOrder(Integer.parseInt(getTagValue("order", eElement)));
+                      
+                      if(eElement.hasAttribute("result")&&getTagValue("result", eElement)!=null)
                       mainAuthor.setAuthorResult(Integer.parseInt(getTagValue("result", eElement)));
+                      
                       mainAuthor.setInterest(getTagValue("interests", eElement));
-		   }
-                   else
+                       if (nNode.getParentNode().getNodeName().equals("main")){
+                           result.setMainAuthor(mainAuthor);
+                       }
+                       else
+                           coauthor.add(mainAuthor);
+                           
+                  } 
                }
+               result.setCoAuthor(coauthor);
               
 		  
 	  } catch (Exception e) {
@@ -61,11 +87,18 @@ public class ReadXML {
     
       private static String getTagValue(String sTag, Element eElement) {
 	NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
- 
+        
         Node nValue = (Node) nlList.item(0);
- 
-	return nValue.getNodeValue();
+        if (nValue!=null)
+            return nValue.getNodeValue();
+        else
+            return null;
   }
   
-    
+//      public static void main(String[] args) {
+//        // TODO code application logic here
+//       Publication test;
+//       test= importPubFromXML("1");
+//       
+//    }
 }
