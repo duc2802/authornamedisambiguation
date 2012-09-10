8697d676d05cbee36342c7096d9233e4b7a76363
tiendo.vn <tiendo.vn@b1b9296c-9d0c-081c-44bb-1419d4cd2576>
2012-09-10 05:28:59 +0000

    make pair paper xml file

---
+++ AND/src/uit/tkorg/and/utils/WriteXMLFile.java
@@ -0,0 +1,167 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.tkorg.and.utils;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+ 
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import uit.tkorg.and.models.Publication;
+
+/**
+ *
+ * @author tiendv
+ */
+public class WriteXMLFile {
+    
+        
+    
+    /**
+     * DTD
+     * <pair>
+     *     <Paper id=1>
+     *          <title> </title>
+     *          <mainauthor>
+     *              <name></name>
+     *              <email></email>
+     *              <interest></interest>
+     *              <affiliation></affiliaciton>
+     *              <result></result>
+     *          </mainauthor>
+     *          <coauthorn> </coauthor>
+     *          <keywords> </keywords>
+     *     </paper>
+     * 
+     *      <Paper id=2>
+     *          <title> </title>
+     *          <mainauthor>
+     *              <name></name>
+     *              <email></email>
+     *              <interest></interest>
+     *              <affiliation></affiliaciton>
+     *              <result></result>
+     *          </mainauthor>
+     *          <coauthorn> </coauthor>
+     *          <keywords> </keywords>
+     *     </paper>
+     * 
+     *     <tag></tag>
+     * <pair> 
+     * 
+     */
+    
+    /**
+     * 
+     * @param publicaitonA
+     * @param publicaitonB
+     * @param name : File name
+     * @param path : path to save file
+     */
+    public void makeXMLFile(Publication publicaitonA, Publication publicaitonB, String name, String path) throws ParserConfigurationException, TransformerConfigurationException, TransformerException 
+    {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.newDocument();
+        
+        Element rootElement = doc.createElement("pair");
+        doc.appendChild(rootElement);
+        
+        //Element paper A
+        Element paperA = makePaperElement(doc, publicaitonA, 1);
+        rootElement.appendChild(paperA);
+        
+        //Element paper B
+        Element paperB = makePaperElement(doc, publicaitonB, 2);
+        rootElement.appendChild(paperB);
+        
+        Element tag = doc.createElement("tag");
+        doc.appendChild(tag);
+        
+        // write the content into xml file
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        String file = path +"\\"+name+".xml";
+        StreamResult result = new StreamResult(new File(file));
+
+        // Output to console for testing
+        // StreamResult result = new StreamResult(System.out);
+        transformer.transform(source, result);
+    }
+    
+    /** 
+     * @param paper
+     * @return Paper element.
+     */
+    public static Element makePaperElement(Document doc, Publication paper, int id)
+    {           
+   
+        Element rootElement = doc.createElement("paper");
+        doc.appendChild(rootElement);
+        Attr attr = doc.createAttribute("id");
+        attr.setValue(Integer.toString(id));
+        rootElement.setAttributeNode(attr);
+        
+        // title elements
+        Element title = doc.createElement("title");
+        title.appendChild(doc.createTextNode(paper.getPubTitle()));
+        rootElement.appendChild(title);
+        
+        // author elements
+        Element mainAuthor = doc.createElement("mainauthor");
+        rootElement.appendChild(mainAuthor);
+        
+        // author name elements
+        Element authorName = doc.createElement("name");
+        authorName.appendChild(doc.createTextNode(paper.getMainAuthor().getAuthorName()));
+        mainAuthor.appendChild(authorName);
+        
+        // email elements
+        Element email = doc.createElement("email");
+        email.appendChild(doc.createTextNode(paper.getMainAuthor().getAuthorEmail()));
+        mainAuthor.appendChild(email);
+        
+        // interest elements
+        Element interest = doc.createElement("interest");
+        interest.appendChild(doc.createTextNode(paper.getMainAuthor().getAuthorInterests()));
+        mainAuthor.appendChild(interest);
+        
+        // affiliation elements
+        Element affiliation = doc.createElement("affiliation");
+        affiliation.appendChild(doc.createTextNode(paper.getMainAuthor().getAuthorAfflication()));
+        mainAuthor.appendChild(affiliation);
+        
+        // affiliation elements
+        Element tagresult = doc.createElement("result");
+        tagresult.appendChild(doc.createTextNode(Integer.toString(paper.getMainAuthor().getAuthorResult())));
+        mainAuthor.appendChild(tagresult);
+        
+        // Coauthors name elements
+        Element coAuthor = doc.createElement("coauthor");
+        String nameCoauthor="";
+        for(int i=0; i<paper.getCoAuthor().size();i++)
+            nameCoauthor = paper.getCoAuthor().get(i).getAuthorName()+", ";
+        coAuthor.appendChild(doc.createTextNode(nameCoauthor));
+        rootElement.appendChild(coAuthor);
+        
+        // Keyword elements
+        Element keywords = doc.createElement("keywords");
+        keywords.appendChild(doc.createTextNode(paper.getPubKeywords()));
+        rootElement.appendChild(keywords);
+        
+        return rootElement;
+        
+    }
+    
+}
